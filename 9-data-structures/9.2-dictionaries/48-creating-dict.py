f92f6b7efe0135062efba9c68e6cc0d321a4cda3
suhas <suhashb.cng@gmail.com>
2026-04-10 09:59:32 +0000

    added dict

--- 9-data-structures/9.2-dictionaries/48-creating-dict.py
+++ 9-data-structures/9.2-dictionaries/48-creating-dict.py
@@ -14,5 +14,5 @@ person = {
 # Different ways to create
 scores = dict(math=95, english=87, science=92)
 
-
+print(scores["english"])  # 87
 # Dictionaries use curly braces {} with key-value pairs separated by colons. Keys must be unique!
